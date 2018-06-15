9bc98e7363d1dc4bf2e444c9c0928f91dfbc906c
Matt Stibbs <mattstibbs@users.noreply.github.com>
2018-06-15 15:07:26 +0100

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "nm-tf-state-bucket"
+    bucket         = "a2si-capacity-terraform-state"
     key            = "dev"
     dynamodb_table = "a2si-capacity-terraform-state"
     region         = "eu-west-2"
