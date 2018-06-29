26a610514cc0789cefe25cdc9921f457bee46653
Geoffrey Hayward <geoffrey.hayward@nhs.net>
2018-06-29 09:42:56 +0100

    Moves the key-pair cert to s3 out of source control

--- key-pairs.tf
+++ key-pairs.tf
@@ -1,4 +1,11 @@
+# Note the contnet type of the object needes to be text/plain
+data "aws_s3_bucket_object" "release_id" {
+  bucket = "uat-certificates"
+  key    = "uat_rsa.pub"
+}
+
+
 resource "aws_key_pair" "key-pair-dev" {
   key_name = "${var.nhs_owner_shortcode}-key-pair-dev"
-  public_key = "${file("key-pair-dev.pub")}"
+  public_key = "${data.aws_s3_bucket_object.release_id.body}"
 }
