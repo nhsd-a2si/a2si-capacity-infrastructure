78d81e4a210593bbff0f3d37c88279fc9418912e
Jonathan Pearce <jonathanpearce@Jonathans-MacBook-Pro.local>
2018-07-16 10:14:15 +0100

    CD-550 - Components to use enhanced health check

--- key-pairs.tf
+++ key-pairs.tf
@@ -1,11 +1,11 @@
 # Note the contnet type of the object needes to be text/plain
 data "aws_s3_bucket_object" "certificate_rsa_pub" {
-  bucket = "${var.environment}-certificates"
+  bucket = "${var.environment}-certificates2"
   key    = "${var.environment}_rsa.pub"
 }
 
 
 resource "aws_key_pair" "key-pair-dev" {
   key_name = "${var.environment}_rsa"
-  public_key = "${data.aws_s3_bucket_object.certificate_rsa_pub.body}"
+  public_key = "${file("key-pair-dev.pub")}"
 }
