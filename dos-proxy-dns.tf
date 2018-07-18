62f89d7b6eb6f195308b4de118af0be326dea50e
Geoffrey Hayward <geoffrey.hayward@nhs.net>
2018-07-18 11:03:23 +0100

    Changes the dos-proxy's LB to lowest health checking sample rate.

--- dos-proxy-dns.tf
+++ dos-proxy-dns.tf
@@ -3,7 +3,7 @@ resource "aws_route53_record" "dos-proxy-lb" {
   type    = "CNAME"
   name    = "${var.dos_proxy_fq_domain_name}"
   records = ["${aws_elastic_beanstalk_environment.dos-proxy-env.cname}"]
-  ttl     = 60
+  ttl     = 5
 }
 
 output "dos_proxy_lb_fqdn" {
