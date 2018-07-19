3fbe7046c0a441ab81c6d5f74cbec68c6506d866
Geoffrey Hayward <geoffrey.hayward@nhs.net>
2018-07-19 09:40:09 +0100

    Adds a health check to Route 53

--- dos-proxy-dns.tf
+++ dos-proxy-dns.tf
@@ -6,6 +6,20 @@ resource "aws_route53_record" "dos-proxy-lb" {
   ttl     = 5
 }
 
+// Needs more work
+resource "aws_route53_health_check" "dos-proxy-heathcheck" {
+  type = "HTTPS_STR_MATCH"
+  port = "443"
+  resource_path = "/heathcheck"
+  fqdn = "${aws_route53_record.dos-proxy-lb.fqdn}"
+  request_interval = "10"
+  failure_threshold = "1"
+  search_string = "UP"
+  tags {
+    Name = "${aws_route53_record.dos-proxy-lb.fqdn}"
+  }
+}
+
 output "dos_proxy_lb_fqdn" {
   value = "${aws_route53_record.dos-proxy-lb.fqdn}"
 }
