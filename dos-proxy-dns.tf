93b029b4d717e289117d3ae8faaa2239aa4a1c51
Geoffrey Hayward <geoffrey.hayward@nhs.net>
2018-07-20 08:24:38 +0100

    Adds automatic failover for DoS proxy

--- dos-proxy-dns.tf
+++ dos-proxy-dns.tf
@@ -4,19 +4,38 @@ resource "aws_route53_record" "dos-proxy-lb" {
   name    = "${var.dos_proxy_fq_domain_name}"
   records = ["${aws_elastic_beanstalk_environment.dos-proxy-env.cname}"]
   ttl     = 5
+  health_check_id = "${aws_route53_health_check.dos-proxy-heathcheck.id}"
+  set_identifier = "Primary"
+  failover_routing_policy {
+    type = "PRIMARY"
+  }
 }
 
-// Needs more work
+resource "aws_route53_record" "dos-proxy-failover" {
+  zone_id = "${data.aws_route53_zone.capacity_hosted_zone.id}"
+  name    = "${var.dos_proxy_fq_domain_name}"
+  records = ["${var.dos_service_domain}"]
+  type    = "CNAME"
+  ttl     = "5"
+
+  set_identifier = "Secondary"
+
+  failover_routing_policy {
+    type = "SECONDARY"
+  }
+
+}
+
 resource "aws_route53_health_check" "dos-proxy-heathcheck" {
   type = "HTTPS_STR_MATCH"
   port = "443"
-  resource_path = "/heathcheck"
-  fqdn = "${aws_route53_record.dos-proxy-lb.fqdn}"
+  resource_path = "/healthcheck"
+  fqdn = "${aws_elastic_beanstalk_environment.dos-proxy-env.cname}"
   request_interval = "10"
   failure_threshold = "1"
   search_string = "UP"
   tags {
-    Name = "${aws_route53_record.dos-proxy-lb.fqdn}"
+    Name = "Dos Proxy Terraform"
   }
 }
 
