021806249c10988b59a8d46a751120cfdc15d65c
Nigel Maddocks <nigel.maddocks@nhs.net>
2018-09-28 16:08:59 +0100

    UAT deployment settings and amendment to the capacity-service url in tf scripts for dos-proxy and info-reader

--- dos-proxy.tf
+++ dos-proxy.tf
@@ -212,7 +212,7 @@ resource "aws_elastic_beanstalk_environment" "dos-proxy-env" {
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
     name = "CAPACITY_SERVICE_CLIENT_API_URL"
-    value = "https://${aws_route53_record.capacity-service-lb.fqdn}/capacity"
+    value = "https://${aws_route53_record.capacity-service-lb.fqdn}"
   }
 
   setting {
