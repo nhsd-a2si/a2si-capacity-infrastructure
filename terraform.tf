d41bbdbe94aea0e8ff6d2c6add695e646b2cc3df
Geoffrey Hayward <geoffrey.hayward@nhs.net>
2018-10-30 14:15:41 +0000

    Merge fix

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "a2si-cd-uat-tf-state"
-    key            = "uat"
-    dynamodb_table = "a2si-cd-uat-tf-state"
+    bucket         = "a2si-cd-geha1-tf-state"
+    key            = "geha1"
+    dynamodb_table = "a2si-cd-geha1-state"
     region         = "eu-west-2"
   }
 }
+
