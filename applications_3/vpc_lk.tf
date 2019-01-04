12e35dc48c94f34e1b2ffc24747ad641211f5b63
Jonathan Pearce <jonathanpearce@Jonathans-MacBook-Pro.local>
2019-01-04 11:52:12 +0000

    tidying up

---
+++ applications_3/vpc_lk.tf
@@ -0,0 +1,68 @@
+data "aws_vpc" "capacity" {
+
+  filter {
+    name   = "tag:Environment"
+    values = ["${var.environment}"]
+    }
+
+  filter {
+    name   = "tag:Name"
+    values = ["Capacity VPC"]
+    }
+}
+
+data "aws_vpc" "reporting" {
+
+  filter {
+    name   = "tag:Environment"
+    values = ["${var.environment}"]
+    }
+
+  filter {
+    name   = "tag:Name"
+    values = ["Reporting VPC"]
+    }
+}
+
+data "aws_subnet_ids" "reporting-public-subnet-ids" {
+  vpc_id = "${data.aws_vpc.reporting.id}"
+}
+
+data "aws_subnet" "reporting-public-subnet" {
+  count = "${length(data.aws_subnet_ids.reporting-public-subnet-ids.ids)}"
+  id    = "${data.aws_subnet_ids.reporting-public-subnet-ids.ids[count.index]}"
+}
+
+data "aws_subnet_ids" "capacity-public-subnet-ids" {
+  vpc_id = "${data.aws_vpc.capacity.id}"
+}
+
+data "aws_subnet" "capacity-public-subnet" {
+  count = "${length(data.aws_subnet_ids.capacity-public-subnet-ids.ids)}"
+  id    = "${data.aws_subnet_ids.capacity-public-subnet-ids.ids[count.index]}"
+}
+
+data "aws_security_groups" "cache-client-sgs" {
+
+  filter {
+    name   = "vpc-id"
+    values = ["${data.aws_vpc.capacity.id}"]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = ["cache-client Security Group"]
+    }
+}
+
+data "aws_security_group" "cache-client-sg" {
+  id = "${data.aws_security_groups.cache-client-sgs.ids[0]}"
+}
+
+data "aws_iam_instance_profile" "a2si-eb" {
+  name = "${var.nhs_owner_shortcode}-a2si-elasticbeanstalk-ec2-role"
+}
+
+data "aws_elasticache_replication_group" "capacity-cache" {
+  replication_group_id = "${var.nhs_owner_shortcode}-cc-gp"
+}
