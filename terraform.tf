f468e765b0699255fa5a9256ab2aa08e328945ef
Jonathan Pearce <jonathanpearce@Jonathans-MacBook-Pro.local>
2019-01-04 10:53:15 +0000

    created applications dir

--- terraform.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "a2si-cd-testp-tf-state"
-    key            = "test-pilot"
-    dynamodb_table = "a2si-cd-testp-tf-state"
-    region         = "eu-west-2"
-  }
-}
